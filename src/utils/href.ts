ea7e5303a9ab8c3b0d1968feb94370ef3a90b830
denisavitski <denisavitski@gmail.com>
2024-11-08 00:49:42 +0300

    feat: Contacts

--- src/utils/href.ts
+++ src/utils/href.ts
@@ -1,13 +1,23 @@
+// Регулярное выражение для телефона (подходит для разных форматов)
+const phoneRegex =
+  /^(\+?\d{1,3}[-.\s]?)?(\(?\d{3}\)?[-.\s]?)?\d{1,4}[-.\s]?\d{2,4}[-.\s]?\d{2,4}$/
+
+// Регулярное выражение для email
+const emailRegex = /^[\w.-]+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,}$/
+
 export function tryCreateHrefFromContact(contact: string) {
-  let href = ''
+  let href: string | undefined
+  let type: 'email' | 'phone' | undefined
 
   if (!href) {
-    if (contact.includes('@')) {
+    if (emailRegex.test(contact)) {
       href = `mailto:${contact.trim()}`
-    } else if (contact.trim().startsWith('+')) {
+      type = 'email'
+    } else if (phoneRegex.test(contact)) {
       href = `tel:${contact.replace(/[^0-9\\.\\+]+/g, '')}`
+      type = 'phone'
     }
   }
 
-  return href
+  return { href, type }
 }
