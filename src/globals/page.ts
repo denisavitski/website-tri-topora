0dd0e66b68587871d7ac08117c13b360cf3950a0
denisavitski <denisavitski@gmail.com>
2024-11-21 18:10:50 +0300

    refactor: morph

--- src/globals/page.ts
+++ src/globals/page.ts
@@ -2,56 +2,32 @@ import { Morph } from 'aptechka/morph'
 
 export const morph = new Morph({
   trailingSlash: true,
+  scrollSelector: '#page-scroll',
 })
-
-morph.preprocessor = (e) => {
-  document.documentElement.classList.add('out')
-  e.resolve()
-}
-
-morph.postprocessor = () => {
-  document.documentElement.classList.remove('out')
-  dispatchEvent(new Event('resize'))
-}
-
-export const scrollElement = {
-  current: null! as HTMLElement,
-}
 
 function updateSize() {
   const clientWidth = document.documentElement.clientWidth
 
-  document.documentElement.style.setProperty(
-    '--inner-height',
-    innerHeight + 'px',
-  )
+  document.documentElement.style.setProperty('--inner-height', innerHeight + 'px')
 
-  document.documentElement.style.setProperty(
-    '--client-width',
-    clientWidth + 'px',
-  )
+  document.documentElement.style.setProperty('--client-width', clientWidth + 'px')
 }
 
 function scrollListener() {
-  if (scrollElement.current.scrollTop > 0) {
+  if (morph.scrollElement.scrollTop > 0) {
     document.documentElement.classList.add('scrolled')
   } else {
     document.documentElement.classList.remove('scrolled')
   }
 }
 
-function updateScrollElement(document: Document) {
-  scrollElement.current?.removeEventListener('scroll', scrollListener)
+document.addEventListener('morphStart', () => {
+  morph.scrollElement.removeEventListener('scroll', scrollListener)
+})
 
-  scrollElement.current = document.querySelector<HTMLElement>('.morph__sheet')!
-
-  scrollElement.current.addEventListener('scroll', scrollListener)
-
+document.addEventListener('morphComplete', () => {
+  morph.scrollElement.addEventListener('scroll', scrollListener)
   scrollListener()
-}
-
-document.addEventListener('morphStart', (e) => {
-  updateScrollElement(e.detail.newDocument)
 })
 
 addEventListener('load', () => {
@@ -59,5 +35,4 @@ addEventListener('load', () => {
   updateSize()
 })
 
-updateScrollElement(document)
 addEventListener('resize', updateSize)
